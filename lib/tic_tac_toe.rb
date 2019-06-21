828b6389270c67749d7947458decf4cb6c5967c9
jessi ortega <ortegabx05@gmail.com>
2019-06-21 16:44:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,8 +47,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board,index)
+    move(board,index)
     display_board(board)
-    move(board,index,current_player)
   else
     turn(board)
   end
