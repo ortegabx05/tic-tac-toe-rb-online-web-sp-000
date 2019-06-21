8682f7e219262449ce496ca4248b4446d95360f4
jessi ortega <ortegabx05@gmail.com>
2019-06-21 16:31:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,11 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  if index.between?(0, 8) && position_taken?(board,index) == false
+    true
+  else 
+    false
+  end
 end
 
 def turn(board)
