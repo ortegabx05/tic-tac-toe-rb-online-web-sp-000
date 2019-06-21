df50a030b9cba7fbeba5f44453e08c1b14375d4d
jessi ortega <ortegabx05@gmail.com>
2019-06-21 16:30:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,8 +25,13 @@ def move(board, index, current_player)
   board[index] = current_player
 end
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def position_taken?(board,index)
+ taken = nil
+ if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+   taken = false
+ else
+   taken = true
+ end
 end
 
 def valid_move?(board, index)
