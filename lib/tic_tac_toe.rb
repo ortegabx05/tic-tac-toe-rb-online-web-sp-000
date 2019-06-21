0bff1c2e929b0cc49898ac48f19e9101d6f4b831
jessi ortega <ortegabx05@gmail.com>
2019-06-21 17:25:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,7 +46,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board)
+  if valid_move?(board,index)
     display_board(board)
   else
     turn(board)
