000890b0bef6c78692c736d64a9c75d18be2a505
jessi ortega <ortegabx05@gmail.com>
2019-06-21 16:24:08 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,116 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  counter = 0
+  until counter == 9
+  counter += 1
+  turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |team|
+    if team == "X" || team == "O"
+      counter += 1
+    end
+end
+return counter
+end
+
+def current_player(board)
+  if turn_count(board)%2 == 0
+    current_player ="X"
+  else
+    current_player ="O"
+end
+return current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+def won?(board)
+winner = nil
+empty_board = board.all? {|x| x == " "}
+WIN_COMBINATIONS.each do |winner_combination|
+    if empty_board
+      return false
+    elsif winner_combination.all? { |value| board[value] =="X" } || winner_combination.all? { |value| board[value] =="O" }
+      winner = winner_combination
+    end
+  end
+  winner
+end
+
+def full?(board)
+   board.all? {|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   else won?(board)
+     false
+   end
+end
+
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
+    return true
+  else 
+    return false
+  end
+end 
+
+def winner(board)
+  if won?(board)
+   board[won?(board)[0]]
+  end
+end
